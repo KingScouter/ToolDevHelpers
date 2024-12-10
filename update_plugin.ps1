42d49aed045d88547f363e63f209bee24180a3f4
Gabriel Ortner <ortner.gabriel@gmail.com>
2024-12-11 00:31:36 +0100

    Added a simple script to quickly update/install the plugin

---
+++ update_plugin.ps1
@@ -0,0 +1,24 @@
+$pluginFolder = ".\Community.PowerToys.Run.Plugin.JSLHelpers\bin\x64\Release\net8.0-windows\"
+$powerToysPluginFolder = "$($env:LOCALAPPDATA)\Microsoft\PowerToys\PowerToys Run\Plugins\JSLHelpers"
+
+Write-Output "Stop PowerToys"
+# Stop PowerToys
+Stop-Process -Name PowerToys
+
+Start-Sleep 2
+
+Write-Output "Remove old plugin"
+# Delete old plugin folder
+if (Test-Path -Path $powerToysPluginFolder) {
+  Remove-Item -Path $powerToysPluginFolder -Recurse
+}
+
+Write-Output "Copy folder"
+# Copy new plugin folder
+Copy-Item -Path $pluginFolder -Destination $powerToysPluginFolder -Recurse
+
+Start-Sleep 2
+
+Write-Output "Start PowerToys"
+# Restart PowerToys
+Start-Process -FilePath "$($env:LOCALAPPDATA)\PowerToys\PowerToys.exe"
